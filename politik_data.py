fd48602de10b7ee4d79b6d854eedc0b669c96c0f
Alberto Ferreira de Melo <73712121+albertoferreirademelo@users.noreply.github.com>
2022-08-28 20:37:43 +0200

    adding all pages

--- politik_data.py
+++ politik_data.py
@@ -1,24 +1,17 @@
-
 '''
-import docx
+Getting data from the budget from the official pdf file
 
-docx_patch = ".\data\mot_202122__4040.docx"
-document = docx.Document(docx_patch)
-tables = document.tables
-for table in tables:
-    for row in table.rows:
-        for cell in row.cells:
-            for paragraph in cell.paragraphs:
-                print(paragraph.text)
-
+moderaternas budgetmotion för 2022 (https://www.riksdagen.se/sv/dokument-lagar/dokument/motion/okad-trygghet-och-fler-som-arbetar---moderaternas_H9024040)
 '''
 
 import tabula
+import pandas as pd
 
+#Moderaternas budgetmotion för 2022
 pdf_path = ".\data\mot_202122__4040.pdf"
 
-dfs = tabula.read_pdf(pdf_path, pages="20", lattice = True)
+dfs = tabula.read_pdf(pdf_path, pages="all", lattice = True)
 
 print (len(dfs))
 
-print (dfs[0])
+print (dfs[-1])
